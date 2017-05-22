f3ed8f9355a5c2ac89dcec83f3e5d73e00445625
sunchengfei02 <sunchengfei02@meituan.com>
2017-05-22 16:39:11 +0800

    整合spring

--- src/main/java/com/my/test/struts2/HelloAction.java
+++ src/main/java/com/my/test/struts2/HelloAction.java
@@ -1,8 +1,11 @@
 package com.my.test.struts2;
 
+import com.my.test.IMyService;
+import com.my.test.UserInfo;
 import com.opensymphony.xwork2.ActionSupport;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by sunchengfei on 2017/5/19.
@@ -19,11 +22,19 @@ public class HelloAction extends ActionSupport {
 
     private String respMessage;
 
+    @Getter
+    private UserInfo userInfo;
+
+    @Autowired
+    private IMyService myServiceImpl;
+
     public String execute() {
         this.name = "xiaoming";
         this.address = "上海";
         this.respCode = "232";
         this.respMessage = "成功";
+
+        this.userInfo = myServiceImpl.selectUser(12);
 
         return SUCCESS;
     }
