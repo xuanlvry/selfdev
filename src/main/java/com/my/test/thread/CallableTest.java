0abe845ec9726d066079f23bdb12e6a1cd90102d
sunchengfei <sunchengfei02@meituan.com>
2018-04-27 10:02:16 +0800

    commit

--- src/main/java/com/my/test/thread/CallableTest.java
+++ src/main/java/com/my/test/thread/CallableTest.java
@@ -6,6 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.*;
 
@@ -16,40 +17,46 @@ import java.util.concurrent.*;
 @ContextConfiguration(locations = {"classpath*:META-INF/application-context.xml", "classpath*:META-INF/application-dao-context.xml"})
 public class CallableTest {
     @Test
-    public void test() {
-        //创建线程池
-        ExecutorService executor = Executors.newFixedThreadPool(10);
-        //创建异步任务列表
-        List<FutureTask<String>> futureTaskss = new ArrayList<FutureTask<String>>();
+    public void test() throws InterruptedException {
+        int threadCount = 5;
 
+        //创建线程池
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        //任务列表
         List<Future<String>> futureTasks = new ArrayList<Future<String>>();
         try {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < threadCount; i++) {
                 //创建异步任务
-//                FutureTask<String> futureTask = new FutureTask<String>(new Worker());
-//                futureTasks.add(futureTask);
-                //执行
-//                executor.submit(futureTask);
-
                 Future<String> futureTask = executor.submit(new Worker());
                 futureTasks.add(futureTask);
             }
 
             //获取值
             for (Future<String> item : futureTasks) {
-                String result = item.get();
+                String result = item.get(10, TimeUnit.MILLISECONDS);
                 System.out.println(result);
             }
         } catch (Exception e) {
 
         } finally {
-            executor.shutdown();
+            System.out.println("main thread shutdonwing " + new Date());
+            executor.shutdownNow();
+            System.out.println("main thread shutdonwed " + new Date());
         }
+
+        Thread.sleep(10000);
     }
 
     private class Worker implements Callable<String> {
         @Override
         public String call() throws Exception {
+            int count = 0;
+            for(int i=0; i<10; i++){
+                Thread.sleep(100);
+                System.out.println(Thread.currentThread().getName() + " working");
+                count++;
+            }
+            System.out.println(Thread.currentThread().getName() + " compleated");
             return Thread.currentThread().getName();
         }
     }
