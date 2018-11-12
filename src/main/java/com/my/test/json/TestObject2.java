9083266f5d1dd329e5b4bd60a87f32524fde0554
sunchengfei <sunchengfei02@meituan.com>
2018-11-12 15:03:59 +0800

    添加布隆过滤器

---
+++ src/main/java/com/my/test/json/TestObject2.java
@@ -0,0 +1,13 @@
+package com.my.test.json;
+
+import lombok.Data;
+
+/**
+ * Created by sunchengfei on 2018/9/29.
+ */
+@Data
+public class TestObject2 {
+    private String name;
+    private Integer age;
+    private String tel;
+}
---
+++ src/main/java/com/my/test/zookeeper/curator/CuratorTest.java
@@ -0,0 +1,7 @@
+package com.my.test.zookeeper.curator;
+
+/**
+ * Created by sunchengfei on 2018/10/16.
+ */
+public class CuratorTest {
+}
