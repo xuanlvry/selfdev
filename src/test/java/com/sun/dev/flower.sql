847d0049a3b09f1c943e7c5e0946815dcce65b89
sunchengfei <sunchengfei02@meituan.com>
2020-05-18 20:10:32 +0800

    修正母亲节时间

--- src/test/java/com/sun/dev/flower.sql
+++ src/test/java/com/sun/dev/flower.sql
@@ -1,7 +1,7 @@
 -- 母亲节 '2019-05-12 00:00:00' and '2019-05-12 23:59:59'
 -- 520 '2019-05-20 00:00:00' and '2019-05-20 23:59:59'
 -- 七夕 '2019-08-07 00:00:00' and '2019-08-07 23:59:59'
--- 母亲节 '2020-05-12 00:00:00' and '2020-05-12 23:59:59'
+-- 母亲节 '2020-05-10 00:00:00' and '2020-05-10 23:59:59'
 select allOrder.CustomerID,
        count(allOrder.NewOrderID)                                                         as '总订单量',
        count(refuse.NewOrderID)                                                           as '拒单量',
@@ -10,7 +10,7 @@ from (select CustomerID, NewOrderID
       from DPF_DeliveryOrder
       where Status != 1
         and Status != 9
-        and ArrivalTime between '2019-08-07 00:00:00' and '2019-08-07 23:59:59') allOrder
+        and ArrivalTime between '2020-05-10 00:00:00' and '2020-05-10 23:59:59') allOrder
          Left JOIN
      (select orders.NewOrderID
       from DPF_DeliveryOrder orders
@@ -19,7 +19,7 @@ from (select CustomerID, NewOrderID
       where orders.Status != 1
         and orders.Status != 9
         and orders.RelationShopId = 0
-        and orders.ArrivalTime between '2019-08-07 00:00:00' and '2019-08-07 23:59:59'
+        and orders.ArrivalTime between '2020-05-10 00:00:00' and '2020-05-10 23:59:59'
         and operation.OperationType = 4) refuse
      on allOrder.NewOrderID = refuse.NewOrderID
 Group by allOrder.CustomerID
@@ -35,7 +35,7 @@ from (select RelationShopId, NewOrderID
       where Status != 1
         and Status != 9
         and RelationShopId != 0
-        and ArrivalTime between '2019-08-07 00:00:00' and '2019-08-07 23:59:59') allOrder
+        and ArrivalTime between '2020-05-10 00:00:00' and '2020-05-10 23:59:59') allOrder
          Left JOIN
      (select orders.NewOrderID
       from DPF_DeliveryOrder orders
@@ -44,7 +44,7 @@ from (select RelationShopId, NewOrderID
       where orders.Status != 1
         and orders.Status != 9
         and orders.RelationShopId != 0
-        and orders.ArrivalTime between '2019-08-07 00:00:00' and '2019-08-07 23:59:59'
+        and orders.ArrivalTime between '2020-05-10 00:00:00' and '2020-05-10 23:59:59'
         and operation.OperationType = 4) refuse
      on allOrder.NewOrderID = refuse.NewOrderID
 Group by allOrder.RelationShopId
