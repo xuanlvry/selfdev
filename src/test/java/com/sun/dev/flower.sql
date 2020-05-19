31bf3a608a978fb2cf4cbe70fa7d4968cb9796fc
sunchengfei <sunchengfei02@meituan.com>
2020-05-19 21:25:55 +0800

    sql增加对比值逻辑；

--- src/test/java/com/sun/dev/flower.sql
+++ src/test/java/com/sun/dev/flower.sql
@@ -5,9 +5,9 @@
 
 -- 接单前，拒单
 select allOrder.CustomerID,
-       count(allOrder.NewOrderID)                                                         as '总订单量',
-       count(refuse.NewOrderID)                                                           as '拒单量',
-       concat(ROUND(count(refuse.NewOrderID) / count(allOrder.NewOrderID) * 100, 2), '%') as '拒单率'
+       count(allOrder.NewOrderID)                                                               as '订单量',
+       count(refuse.NewOrderID) - 10                                                            as '拒单量对比值',
+       concat(ROUND(count(refuse.NewOrderID) / count(allOrder.NewOrderID) * 100, 2) - 0.3, '%') as '拒单率对比值'
 from (select CustomerID, NewOrderID
       from DPF_DeliveryOrder
       where Status != 1
@@ -29,10 +29,10 @@ order by allOrder.CustomerID asc
 limit 0, 10000;
 
 -- 接单后，拒单
-select allOrder.RelationShopId                                                            as 'shopId',
-       count(allOrder.NewOrderID)                                                         as '总订单量',
-       count(refuse.NewOrderID)                                                           as '拒单量',
-       concat(ROUND(count(refuse.NewOrderID) / count(allOrder.NewOrderID) * 100, 2), '%') as '拒单率'
+select allOrder.RelationShopId                                                                  as '点评shopID',
+       count(allOrder.NewOrderID)                                                               as '订单量',
+       count(refuse.NewOrderID) - 5                                                             as '拒单量对比值',
+       concat(ROUND(count(refuse.NewOrderID) / count(allOrder.NewOrderID) * 100, 2) - 0.3, '%') as '拒单率对比值'
 from (select RelationShopId, NewOrderID
       from DPF_DeliveryOrder
       where Status != 1
@@ -53,6 +53,14 @@ from (select RelationShopId, NewOrderID
 Group by allOrder.RelationShopId
 order by allOrder.RelationShopId asc
 limit 0, 10000;
+
+-- 鲜花联盟
+select count(NewOrderID)
+from DPF_DeliveryOrder
+where Status != 1
+  and Status != 9
+  and ArrivalTime between '2020-05-20 00:00:00' and '2020-05-20 23:59:59'
+  and CustomerID =;
 
 -- 拒单
 select count(allOrder.NewOrderID)                                                         as '总订单量',
@@ -95,7 +103,8 @@ from (select NewOrderID, CustomerID
         and orders.Status != 9
         and orders.ArrivalTime between '2020-05-10 00:00:00' and '2020-05-10 23:59:59'
         and orders.CustomerID IN (41062815, 40512776, 41521960, 40874484)
-        and operation.OperationType = 4) refuse
+        and operation.OperationType = 4
+        and operation.AddTime between '2020-05-10 07:00:00' and '2020-05-10 07:30:00') refuse
      on allOrder.NewOrderID = refuse.NewOrderID
 Group by allOrder.CustomerID;
 
@@ -120,6 +129,30 @@ from (select NewOrderID, RelationShopId
         and orders.ArrivalTime between '2020-05-10 00:00:00' and '2020-05-10 23:59:59'
         and orders.RelationShopId IN (1674057538, 131453998, 110493438, 21072788, 66955598)
         and operation.OperationType = 4) refuse
+     on allOrder.NewOrderID = refuse.NewOrderID
+Group by allOrder.RelationShopId;
+
+select allOrder.RelationShopId                                                            as shopId,
+       count(allOrder.NewOrderID)                                                         as '总订单量',
+       count(refuse.NewOrderID)                                                           as '拒单量',
+       concat(ROUND(count(refuse.NewOrderID) / count(allOrder.NewOrderID) * 100, 2), '%') as '拒单率'
+from (select NewOrderID, RelationShopId
+      from DPF_DeliveryOrder
+      where Status != 1
+        and Status != 9
+        and ArrivalTime between '2020-05-10 00:00:00' and '2020-05-10 23:59:59'
+        and RelationShopId IN (1674057538, 131453998, 110493438, 21072788, 66955598)) allOrder
+         Left JOIN
+     (select orders.NewOrderID
+      from DPF_DeliveryOrder orders
+               inner join DPF_DeliveryOrderOperation operation
+                          ON orders.NewOrderID = operation.NewOrderID
+      where orders.Status != 1
+        and orders.Status != 9
+        and orders.ArrivalTime between '2020-05-10 00:00:00' and '2020-05-10 23:59:59'
+        and orders.RelationShopId IN (1674057538, 131453998, 110493438, 21072788, 66955598)
+        and operation.OperationType = 4
+        and operation.AddTime between '2020-05-10 07:00:00' and '2020-05-10 07:30:00') refuse
      on allOrder.NewOrderID = refuse.NewOrderID
 Group by allOrder.RelationShopId;
 
